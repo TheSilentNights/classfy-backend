b1b8da498389b7eabe99fd2a6e666c3fa57071e9
TheSilentNights <97817549+TheSilentNights@users.noreply.github.com>
2025-06-19 22:58:33 +0800

    fix problems and add requirements.txt

--- src/classify.py
+++ src/classify.py
@@ -1,6 +1,20 @@
 from flask import current_app
 import os
+from bert import bert_tuili
+from src.ocr import recognize
+
 
 class Classify:
-    def __init__(self,saved_file_name):
-        path_to_file = os.path.join(current_app.config['UPLOAD_FOLDER'],saved_file_name)
+    def __init__(self, saved_file_name):
+        self.path_to_file = os.path.join(current_app.config['UPLOAD_FOLDER'], saved_file_name)
+
+    def classify(self):
+        text_content = recognize(image_path=self.path_to_file)
+        bert_tuili.start(text_content)
+
+
+if __name__ == '__main__':
+    os.environ['HF_ENDPOINT'] = "https://hf-mirror.com"
+    path_to_img = "D:/web/project/classfy-backend/test/page_1.png"
+    classifier = Classify(path_to_img)
+    classifier.classify()
--- src/ocr.py
+++ src/ocr.py
@@ -1,17 +1,20 @@
 from paddleocr import PaddleOCR
 
 # 初始化PaddleOCR实例（使用PP-OCRv4模型）
-ocr = PaddleOCR(
-    use_doc_orientation_classify=True, # 启用方向分类
-    lang='ch',           # 中文识别（支持中英文混合）      # 根据环境选择是否使用GPU
-    ocr_version='PP-OCRv4',  # 指定使用v4模型       # 关闭日志输出
-)
 
-def recognize_and_print(image_path):
+
+def recognize(image_path):
     """
     识别图片中的文字并打印到终端
     :param image_path: 图片路径
     """
+
+    ocr = PaddleOCR(
+        use_doc_orientation_classify=True, # 启用方向分类
+        lang='ch',           # 中文识别（支持中英文混合）      # 根据环境选择是否使用GPU
+        ocr_version='PP-OCRv4',  # 指定使用v4模型       # 关闭日志输出
+    )
+
     # 执行OCR识别
     result = ocr.predict(image_path)
 
@@ -20,9 +23,9 @@ def recognize_and_print(image_path):
     for res in result:
         for text in res["rec_texts"]:
             result_text = result_text + text
-    print(result_text)
+    return result_text
 
 if __name__ == "__main__":
     # 替换为你的图片路径
     image_path = 'page_1.png'  # 或使用绝对路径如：r'C:\path\to\image.jpg'
-    recognize_and_print(image_path)
+    recognize(image_path)
